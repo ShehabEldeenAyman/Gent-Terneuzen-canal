53c913132b6d05286276159ef7a45a80b4f8a94f
ShehabEldeenAyman <shehab.ayman@vito.be>
2026-04-02 10:51:12 +0200

    .

--- RDF2TSS_V2/RDF2TSS_V2.py
+++ RDF2TSS_V2/RDF2TSS_V2.py
@@ -40,9 +40,12 @@ def create_sensor_set(graph):
     }
     '''
     print('Started identifying unique sensors...')
+    print('faaaaa')
     for sensor in graph.query(get_sensor_query):
         sensor_term = sensor[0]
         sensor_set.add(sensor_term)
+        print(f'Identified sensor: {sensor_term}')
+        print('help')
 
     print(f'{len(sensor_set)} Sensors identified successfully.')
     return sensor_set
