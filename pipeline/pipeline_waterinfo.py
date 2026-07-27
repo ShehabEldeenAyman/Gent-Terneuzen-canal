aefe4f29388611db5f2455e1f628e8ce1760437b
ShehabEldeenAyman <shehab.ayman@vito.be>
2026-07-27 14:26:51 +0200

    full integration of automated alignments into the pipeline

--- pipeline/pipeline_waterinfo.py
+++ pipeline/pipeline_waterinfo.py
@@ -1,6 +1,7 @@
 import pipeline_core as core
 import sys; sys.path.append('..')  # Adds the parent directory
 import python_backend_server.constants as constants
+from rdflib import Graph,Namespace,URIRef,Literal
 
 def main():
     core.setup_environment()
@@ -12,7 +13,10 @@ def main():
         core.step_1_fetch_data(constants.START_DATE, constants.END_DATE,value, parameter_name=key)
         core.step_2_preprocess(parameter_name=key)
         core.step_3_rml_mapping(parameter_name=key)
-        core.step_4_ingest_virtuoso(f"../data/{key}.ttl", constants.GRAPH_URI, delete_existing=True)
+        print("--- Automated Aligments started ---")
+        core.step_3_5_automating_alignments(f"../data/{key}.ttl",URIRef("http://qudt.org/vocab/unit/MilliS-PER-CentiM"))
+        print("--- Automated Aligments finished ---")
+        core.step_4_ingest_virtuoso(f"../data/{key}.ttl", constants.GRAPH_URI, delete_existing=False)
         core.step_5_rdf2tss(f"../data/{key}.ttl", f"../data/{key}_tss.ttl",f"Data/{key}")
 
 if __name__ == "__main__":
